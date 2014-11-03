76cd5f9b5add10b09018fc1a6f9c942129d1e384
darkknightzh <zhanghu0722@gmail.com>
2014-11-03 22:43:54 +0800

    Update Microsoft_Source_Reader.cpp

--- Microsoft_Source_Reader.cpp
+++ Microsoft_Source_Reader.cpp
@@ -2,6 +2,7 @@
 // a simple code of the usage of Microsoft Source Reader
 // Welcome to point out the mistakes of the program
 // Forgive me for forget the references of the code. 
+// Some code comes from : http://msdn.microsoft.com/en-us/library/windows/desktop/dd389281(v=vs.85).aspx
 // Meanwhile thanks Microsoft
 // Writen by darkknightzh
 // 2014.11.3
